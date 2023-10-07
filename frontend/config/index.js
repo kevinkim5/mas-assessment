df232c838e56972b8589236efe25f1c46a0b9faf
kevinkim <kevinnkimm96@gmail.com>
2023-10-08 00:57:44 +0800

    Add frontend config files

---
+++ frontend/config/index.js
@@ -0,0 +1,18 @@
+export default {
+	dev: {
+		baseURL: "/",
+		apiBaseURL: "/",
+	},
+	cloud: {
+		baseURL: "/",
+		apiBaseURL: "/",
+	},
+	uat: {
+		baseURL: "/",
+		apiBaseURL: "/",
+	},
+	prod: {
+		baseURL: "/",
+		apiBaseURL: "/",
+	},
+};
