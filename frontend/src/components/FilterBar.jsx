ff6c9e6406204ad8d14bbb90026085a1cab41e9b
kevinkim <kevinnkimm96@gmail.com>
2023-10-08 11:40:19 +0800

    Add FilterBar

---
+++ frontend/src/components/FilterBar.jsx
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import RoomsContext from "../roomsContext";
+
+const selectOptions = ["Title", "Location", "Bedrooms", "Bathrooms"];
+
+export default function FilterBar() {
+	const { setSort, sort } = useContext(RoomsContext);
+
+	const handleChange = (e) => setSort(e.target.value);
+
+	return (
+		<div className="filter-bar">
+			<FormControl
+				size="small"
+				sx={{ m: 1, minWidth: 120 }}
+				style={{ marginLeft: 0 }}
+			>
+				<InputLabel id="select-label">Sort</InputLabel>
+				<Select
+					id="sort"
+					label="sort"
+					labelId="select-label"
+					onChange={handleChange}
+					value={sort}
+				>
+					<MenuItem value="">
+						<em>None</em>
+					</MenuItem>
+					{selectOptions.map((item) => {
+						return (
+							<MenuItem key={item} value={item.toLowerCase()}>
+								{item}
+							</MenuItem>
+						);
+					})}
+				</Select>
+			</FormControl>
+		</div>
+	);
+}
